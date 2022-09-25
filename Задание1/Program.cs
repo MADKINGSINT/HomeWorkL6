5d418fb39e6d584a173454305eae7aabea14ffd6
madkings <unconfigured@null.spigotmc.org>
2022-09-25 12:28:28 +0300

    Programs writed

---
+++ Задание1/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+int[]  array;
+int NumbersMoreZero=0;
+int[] WriteNumbers()
+{
+  
+System.Console.Write("сколько чисел вы хотите ввести? ");
+
+int HowMuchNumbers = Convert.ToInt32(Console.ReadLine());
+
+ array = new int [HowMuchNumbers];
+    for(int i = 0; i <= HowMuchNumbers - 1; i++)
+    {
+System.Console.Write("введите число:   ");
+int numbers = Convert.ToInt32(Console.ReadLine());
+if (numbers > 0)
+{
+    NumbersMoreZero++;
+}
+array[i] = numbers;
+}
+return array;
+
+}
+void PrintArray(int[] array)
+     {
+foreach (var item in array) {
+            Console.Write($"{item}, ");
+            
+            }
+            
+}
+WriteNumbers();
+PrintArray(array);
+System.Console.WriteLine($" введено чисел больше нуля: {NumbersMoreZero}");
+System.Console.WriteLine(" ");
---
+++ Задание2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+double b1, b2;
+double k1, k2; 
+System.Console.WriteLine("введите b1:  ");
+b1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите b2:  ");
+b2 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите k1:  ");
+k1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("введите k2:  ");
+k2 = Convert.ToInt32(Console.ReadLine());
+
+void TochkaPeresecheniya(double b1, double b2, double k1 ,double  k2)
+{
+double x = (b1 - b2 ) / (k2 - k1);
+ double y = (k1 * x +b1);
+if ( k1 == k2 )
+{
+Console.WriteLine("прямые не пересекаются");
+          
+}
+else 
+{
+Console.WriteLine($"точка пересечения прямых {x}, {y}");
+  }
+
+
+
+
+
+}
+
+TochkaPeresecheniya(b1, b2, k1, k2);
